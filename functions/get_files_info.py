e6140f155b2eb6b6e22b32184e8d3dc6fb8b7cb0
jaystev242 <jstevens5753@gmail.com>
2026-08-06 10:30:18 +1000

    get_files check

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,13 +1,16 @@
 import os
 def get_files_info(working_directory: str, directory: str = ".") -> str:
     try:
-        working_directory_abs = os.path.abspath()
+        working_directory_abs = os.path.abspath(working_directory)
         target_dir = os.path.normpath(os.path.join(working_directory_abs, directory))
         valid_target_dir = os.path.commonpath([working_directory_abs, target_dir]) == working_directory_abs
-        if valid_target_dir:
-            return f'Success: "{directory}" is within the working directory'
-    except:
-        if not valid_target_dir:
-            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-        if not directory:
-            return f'Error: "{directory}" is not a directory'
+        if os.path.isdir(target_dir) == False:
+            return f'Error: "{target_dir}" is not a directory'
+        else:
+            if valid_target_dir == True:
+               # return f'Success: "{directory}" is within the working directory'
+                
+            else:
+                return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+    except OSError as e:
+        return f"Error: {e}"
