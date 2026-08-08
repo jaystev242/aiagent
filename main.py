de1b1483a05d518072eac754264e368ce5ee6251
jaystev242 <jstevens5753@gmail.com>
2026-08-08 21:08:58 +1000

    bug fixed

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-def main():
-    import os
-    import json
-    from dotenv import load_dotenv
-    import argparse
-    from prompts import system_prompt
-    from call_functions import available_functions, call_function
+import os
+import json
+import sys
+from dotenv import load_dotenv
+import argparse
+from prompts import system_prompt
+from call_functions import available_functions, call_function
 
+def main():
     load_dotenv()
     api_key = os.environ.get("OPENROUTER_API_KEY")
 
@@ -33,31 +34,39 @@ def main():
         {"role": "user", "content": args.user_prompt},
     ]
 
-    response = client.chat.completions.create(
-        model=model, 
-        messages=messages,
-        tools=available_functions,)
+    for _ in range(20):
+        response = client.chat.completions.create(
+            model=model, 
+            messages=messages,
+            tools=available_functions,)
 
-    message = response.choices[0].message
+        message = response.choices[0].message
+        messages.append(message)
 
-    if message.tool_calls:
-        for tool_call in message.tool_calls:
-            result_message = call_function(tool_call, args.verbose)
-            if result_message["content"] == None or result_message["content"] == "":
-                raise Exception("No tool call content")
-            if args.verbose:
-                print(f"-> {result_message['content']}")
-    else:
-            print(response.choices[0].message.content)
-
-    if args.verbose:
-        if response.usage == None:
-            raise RuntimeError("Failed API request")
+        if message.tool_calls:
+            for tool_call in message.tool_calls:
+                result_message = call_function(tool_call, args.verbose)
+                messages.append(result_message)
+                if result_message["content"] == None or result_message["content"] == "":
+                    raise Exception("No tool call content")
+                if args.verbose:
+                    print(f"-> {result_message['content']}")
         else:
-            print(f"User prompt: {messages[0]["content"]}")
-            print(f"Prompt tokens: {response.usage.prompt_tokens}")
-            print(f"Response tokens: {response.usage.completion_tokens}")
-            print(f"Response:\n{response.choices[0].message.content}")
+                print(response.choices[0].message.content)
+                return
+
+        if args.verbose:
+            if response.usage == None:
+                raise RuntimeError("Failed API request")
+            else:
+                print(f"User prompt: {messages[1]["content"]}")
+                print(f"Prompt tokens: {response.usage.prompt_tokens}")
+                print(f"Response tokens: {response.usage.completion_tokens}")
+                print(f"Response:\n{response.choices[0].message.content}")
+
+    print("Error:  Maximum iterations reached")
+    sys.exit(1)
+        
 
 if __name__ == "__main__":
     main()
